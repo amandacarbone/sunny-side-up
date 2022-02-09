28e75206510bfcc80e2fdd8d2b3865400d7fe1c2
Amanda Carbone <amandamooney0418@gmail.com>
2022-02-09 18:23:42 -0500

    remove button works in cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,6 +5,15 @@ export function Cart() {
 
     const { orders, removeItem } = useContext(OrderContext);
 
+    function showRemoveButton() {
+        let foundItem = orders.find((item) => item.id)
+        if(foundItem) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     return (
         <div className="cart">
             <h2>Cart</h2>
@@ -12,6 +21,8 @@ export function Cart() {
                 <ul key={order.id}>
                     <li>{order.name}</li>
                     <li>${order.price}</li>
+                    <button className={showRemoveButton() === true ? "show" : "hide"} 
+                    onClick={() => removeItem(order.id)}>Remove</button>
                 </ul>)}
         </div>
     );
