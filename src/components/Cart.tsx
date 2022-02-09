8f53e35b2d3f82efcbc057a81cfa834f6d251979
Amanda Carbone <amandamooney0418@gmail.com>
2022-02-09 18:17:36 -0500

    orders are rendered in cart when added

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,11 +3,16 @@ import OrderContext from "../contexts/OrderContext";
 
 export function Cart() {
 
-    const { orders } = useContext(OrderContext);
+    const { orders, removeItem } = useContext(OrderContext);
 
     return (
         <div className="cart">
             <h2>Cart</h2>
+            {orders.map(order =>
+                <ul key={order.id}>
+                    <li>{order.name}</li>
+                    <li>${order.price}</li>
+                </ul>)}
         </div>
     );
 
